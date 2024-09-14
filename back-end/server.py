ff0af4d3efd287dea6183b6ebeffa2bf4f065b02
Ryan Erickson <63575432+ryan-erickson-dev@users.noreply.github.com>
2024-09-14 17:14:34 -0400

    Added client queue and driver data structure to backend server.

--- back-end/server.py
+++ back-end/server.py
@@ -1,14 +1,54 @@
-from flask import Flask
+from flask import Flask, request
 import sys, os
 
 app = Flask(__name__)
 
+'''
+Queue data Structure:
+Stores information about each client that enters
+the queue. Used to calculate route information.
+[
+    {
+        "rider_name": "Ryan",
+        "passenger_count": 3,
+        "current_address": "... Blacksburg, VA 24060",
+        "destination":" ... Blacksburg, VA 24060"
+    },
+    {
+        "rider_name":"Brandon",
+        "passenger_count": 3,
+        "current_address": "... Blacksburg, VA 24060",
+        "destination":" ... Blacksburg, VA 24060",
+    },
+ ...]
+
+ QUEUE.append() to enqueue riders.
+ QUEUE.pop(0) to dequeue riders.
+'''
+QUEUE = [{}]
+
+'''
+Driver List:
+Stores information about each driver and assigns them a
+queue within the web application.
+
+[
+    {
+        "driver_name": "Ryan",
+        "car_make": "Nissan",
+        "car_color": "white",
+        "car_capacity": 5,
+        "driver_queue": QUEUE,
+    }
+]
+'''
+DRIVERS = [{}]
 
 def usage_error():
     print(f"ERROR: Usage: python {os.path.basename(__file__)} [api_key: str]", file=sys.stderr)
     sys.exit(1)
 
-@app.route('/')
+@app.route('/data')
 def index():
     return "This is the root index returned."
 
